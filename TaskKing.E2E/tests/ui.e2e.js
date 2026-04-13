6d0bd5507d947398fc203c23a3a8c934beb49638
thoknu <144660048+thoknu@users.noreply.github.com>
2026-04-13 17:09:26 +0200

    chore: add e2e testing with testcafe

---
+++ TaskKing.E2E/tests/ui.e2e.js
@@ -0,0 +1,23 @@
+﻿import { Selector } from "testcafe";
+
+const baseUrl = process.env.BASE_URL;
+
+fixture("TaskKing UI E2E")
+    .page(baseUrl);
+
+test("create task and verify it appears in list", async t => {
+
+    const input = Selector('#taskTitle');
+    const form = Selector('#taskForm');
+    const list = Selector('#tasks');
+
+    const title = `ui-task-${Date.now()}`;
+
+    await t
+        .typeText(input, title)
+        .click(form.find('button'));
+
+    const taskItem = list.find('li').withText(title);
+
+    await t.expect(taskItem.exists).ok({ timeout: 5000 });
+});
