9c938c1c4e9b00be1f806488f174fe330ec126dc
thoknu <144660048+thoknu@users.noreply.github.com>
2026-04-13 10:20:43 +0200

    chore: added k6 test for get tasks

---
+++ k6/get-tasks-test.js
@@ -0,0 +1,20 @@
+﻿import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 5,
+    duration: '10s',
+};
+
+export default () {
+    const baseUrl = __ENV.BASE_URL;
+    
+    const res = http.get(`${baseUrl}/api/tasks`);
+    
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'response time < 500ms': (r) => r.timings.duration < 500,
+    });
+    
+    sleep(1);
+}
