b35f9f4991fd2ebe087a88f541c4ff055101742a
thoknu <144660048+thoknu@users.noreply.github.com>
2026-04-15 14:49:52 +0200

    fix: require to addCommentDto to avoid underposting

--- server/TaskKing.Api/DTOs/AddCommentDto.cs
+++ server/TaskKing.Api/DTOs/AddCommentDto.cs
@@ -1,7 +1,12 @@
-﻿namespace TaskKing.Api.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskKing.Api.DTOs;
 
 public class AddCommentDto
 {
+    [Required]
     public string Content { get; set; } = string.Empty;
+
+    [Required]
     public int TaskId { get; set; }
 }
